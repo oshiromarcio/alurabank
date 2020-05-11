57aec5af318674665f66bc32eb7a47fffdd40bfc
Marcio Oshiro <marciooshiro@Mac-mini-de-Marcio.local>
2020-05-11 16:52:54 -0300

    Aulas 1 e 2 Completos

---
+++ app/js/models/Negociacao.js
@@ -0,0 +1,16 @@
+class Negociacao {
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
+    }
+    get data() {
+        return new Date(this._data);
+    }
+    get quantidade() {
+        return this._quantidade;
+    }
+    get valor() {
+        return this._valor;
+    }
+}
